83516a3b816c19b0ade8c21a80e9c8c51b843583
xxxl71y <xxxly205810@gmail.com>
2026-09-20 20:19:36 +0800

    DDL看板: 移除分类筛选栏，主页拆分为进行中+历史事件两区域

--- 倒计时看板/sw.js
+++ 倒计时看板/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ddl-board-v2";
+const CACHE_NAME = "ddl-board-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
