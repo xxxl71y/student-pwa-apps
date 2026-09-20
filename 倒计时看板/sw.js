1116ded5f1d321abb06bee3f195aef4b6441bec3
xxxl71y <xxxly205810@gmail.com>
2026-09-20 20:10:52 +0800

    refactor: 记账本Tab分区+年度统计+回到本月按钮; DDL看板大倒计时+整卡着色+精确时间

--- 倒计时看板/sw.js
+++ 倒计时看板/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ddl-board-v1";
+const CACHE_NAME = "ddl-board-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
--- 记账本/sw.js
+++ 记账本/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ledger-v1";
+const CACHE_NAME = "ledger-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
