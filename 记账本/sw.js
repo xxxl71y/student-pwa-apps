128fe7f81a4d80b9597918a1a55fa993d215b98e
xxxl71y <xxxly205810@gmail.com>
2026-09-20 21:22:24 +0800

    记账本: 页面固定不滚动，仅内容区域可滚动

--- 记账本/sw.js
+++ 记账本/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ledger-v2";
+const CACHE_NAME = "ledger-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
