827d108fb85237cd46cea4ac240eff3471e44f8a
xxxl71y <xxxly205810@gmail.com>
2026-09-20 20:26:42 +0800

    DDL看板: Tab切换进行中/历史事件，页面固定不滚动仅列表区域可滚动

--- 倒计时看板/sw.js
+++ 倒计时看板/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ddl-board-v3";
+const CACHE_NAME = "ddl-board-v4";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
